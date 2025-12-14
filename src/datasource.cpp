52c62739bea50f07a774920b556d5cc886faa2ae
Adrien Sporrer <47658097+Adrienspo@users.noreply.github.com>
2025-12-14 18:01:00 +0100

    chore: adjust clang-format constructor-related rules

--- src/datasource.cpp
+++ src/datasource.cpp
@@ -3,4 +3,9 @@
 
 #include "datasource.h"
 
-DataSource::DataSource(QString name, QString hostname, const int port, QString username, QString password) : m_name(std::move(name)), m_hostname(std::move(hostname)), m_port(port), m_username(std::move(username)), m_password(std::move(password)) {}
+DataSource::DataSource(QString name, QString hostname, const int port, QString username, QString password) :
+    m_name(std::move(name)),
+    m_hostname(std::move(hostname)),
+    m_port(port),
+    m_username(std::move(username)),
+    m_password(std::move(password)) {}
